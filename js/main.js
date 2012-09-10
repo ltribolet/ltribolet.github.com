07159b7483889066a1cc3e15717e0be01c7096f5
Luc <luc.tribolet@gmail.com>
2012-09-10 17:59:57 +0100

    Step 1
    All the PSD integrated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+$(function() {
+	var meterSkills = $(".skills .meter");
 
+	$(window).scroll(function () {
+
+		meterSkills.each(function ( index, elt) {
+			$elt = $(elt);
+
+			$progress = $($elt.children("span"));
+
+			var eltPosition = $elt.offset();
+
+			if ($(window).height() + $(window).scrollTop() >= eltPosition.top && $progress.data("size") !== 0) {
+				$progress.animate({
+					"width": $progress.data("size")+"%"
+				},{duration: 1500,easing: "easeInOutQuart"});
+				$progress.data("size", 0);
+			}
+		});
+		
+	});
+
+	$(".ribbon-form").on("click", function () {
+		$(".contact").slideToggle();
+	})
+});
