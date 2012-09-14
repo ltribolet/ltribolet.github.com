a1fe2aaa04f74c2c924e3e8b188ff7ba8a8e92cf
Luc <luc.tribolet@gmail.com>
2012-09-14 12:13:34 +0100

    New changes
    Refactore dropdown contact form
    Refactore experiences as slidetoggle one by one

--- js/main.js
+++ js/main.js
@@ -8,10 +8,57 @@ $(function() {
 	});
 
 	$(".ribbon-form").on("click", function () {
-		if($(this).hasClass("notactive"))
-			$(this).removeClass("notactive");
-		else $(this).addClass("notactive");
-		$(".contact").slideToggle();
+
+		var $sibl = $(this).siblings();
+		if($sibl.hasClass("notactive"))
+			$sibl.removeClass("notactive");
+		else $sibl.addClass("notactive");
+		$(".contact .slide").slideToggle();
+	});
+
+	$('body').on('click.collapse.data-api', '[data-toggle=collapse]', function (e) {
+		$this = $(this);
+
+		if($this.hasClass("collapsed")) {
+			$this.html("+");
+		}
+		else $this.html("&dash;");
+	});
+
+	$('a.checkpoint').click(function (e) {
+		e.preventDefault();
+
+		var $this = $(this),
+		selector = $this.attr('data-target'),
+		$previous,
+		$target;
+
+		if (!selector) {
+			selector = $this.attr('href');
+			selector = selector && selector.replace(/.*(?=#[^\s]*$)/, ''); //strip for ie7
+		}
+
+		$previous = $("#experiences .active");
+		$target = $(selector);
+
+		if($previous.length && $previous[0] !== $target[0]) {
+			$previous.removeClass("active").slideToggle("1500", function() {
+				$target.addClass("active").slideToggle();
+			});
+		}
+		else if($previous.length && $previous[0] === $target[0]){
+			$target.slideToggle("1500", function() {
+				$(this).removeClass("active");
+			});
+		}
+		else {
+			$target.slideToggle("1500", function() {
+				$(this).addClass("active");
+			});
+		}
+
+		
+
 	});
 
 	function meterProgress() {
